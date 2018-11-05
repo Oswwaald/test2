c62078b471773030a9e4427a3fb25742482ceda8
Oswwaald <43680214+Oswwaald@users.noreply.github.com>
2018-11-05 16:59:19 -0500

    Update NameServiceInterface.java

--- Interface/NameServiceInterface.java
+++ Interface/NameServiceInterface.java
@@ -6,6 +6,6 @@ import java.rmi.RemoteException;
 
 public interface NameServiceInterface extends Remote {
 	boolean verifyDispatcher(String username, String password) throws RemoteException;
-	HashMap<CalculatorInterface, Integer> getCalculators(String username, String password) throws RemoteException;
-	void setCalculator(String calculator, int capacity, int port) throws RemoteException ;
+	HashMap<String, Configuration> getCalculators(String username, String password) throws RemoteException;
+	void setCalculator(String name, Configuration configuration) throws RemoteException ;
 }
