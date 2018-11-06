00db36a172988c902d9e579d8ad31ea7f554be20
Oswwaald <43680214+Oswwaald@users.noreply.github.com>
2018-11-06 11:05:12 -0500

    Update Dispatcher.java

--- Dispatcher/Dispatcher.java
+++ Dispatcher/Dispatcher.java
@@ -12,9 +12,7 @@ public class Dispatcher {
 	private NameServiceInterface nameServiceStub;
 	private HashMap <CalculatorInterface, Integer> calculatorsStub; 
 	private ArrayList<Configuration> calculatorInformation;
-
 	private List<String> fileLines = new ArrayList<String>();
-	
 	public static String dispatcherFile;
 	public static String dispatcherUsername;
 	public static String dispatcherPassword;
@@ -24,12 +22,11 @@ public class Dispatcher {
 	 * Recuperation des arguments fournis dans la commande.
 	 */
 	public static void main(String[] args) {
-		//Enregistrement de l'identifiants et du mot de passe en variable local.
+		// Enregistrement de l'identifiants et du mot de passe en variable local.
 		dispatcherFile = args[0];
 		dispatcherUsername = args[1];
 		dispatcherPassword = args[2];
 		dispatcherSecureMode = Boolean.parseBoolean(args[3]);
-		
 		try {
 			Dispatcher dispatcher = new Dispatcher(dispatcherFile,dispatcherUsername,dispatcherPassword,dispatcherSecureMode);
 			dispatcher.run();
@@ -39,7 +36,7 @@ public class Dispatcher {
 	}
 	
 	public Dispatcher(String file, String username, String password, boolean secureMode) throws IOException {
-				
+		
 		// Repartition du contenu du fichier en ligne dans la liste fileLines.
 		String line;
 		BufferedReader br = new BufferedReader(new FileReader(file));
@@ -53,15 +50,14 @@ public class Dispatcher {
 		// Hypothese de simplification : NameService a une adresse IP fixe (172.0.0.1).
 		nameServiceStub = loadNameServiceStub("127.0.0.1");
 		
-				
-		// Recuperer de la liste des calculators references dans NameService.
+		// Recuperation de la liste des calculators de NameService.
 		try {
 			calculatorInformation = nameServiceStub.getCalculators(dispatcherUsername, dispatcherPassword);
 		}
 		catch (RemoteException e) {
 			System.out.println("Erreur: " + e.getMessage());
 		}
-		// Definition des stubs de Calculators.
+		// Recuperation des stubs de Calculators.
 		calculatorsStub = loadCalculatorStub(calculatorInformation);
 		
 		
@@ -86,11 +82,11 @@ public class Dispatcher {
 	}
 	
 	/*
-	 * Calcul du fichier dans le mode sécurisé. 
+	 * Calcul du fichier dans le mode securise. 
 	 */
 	private void secureCalculation(String username, String password) {
 		
-		//Inititialisation du résultat final
+		//Inititialisation du resultat final
 		int resultat = 0;
    
 
@@ -120,12 +116,12 @@ public class Dispatcher {
 				{
 				
 				// L'erreur -1 met en evidence un probleme de syntaxe dans le contenu du fichier (notamment sur la composition des operations).
-				// Interruption du calcul et renvoie de message à l'utilisateur.
+				// Interruption du calcul et renvoie de message a l'utilisateur.
 				case -1:
 					System.out.println("Calcul impossible : Revoir le contenu du fichier transmis.");
 					System.exit(1);
 				
-				// L'erreur -2 met en evidence un problème de surcharge du Calculator. 
+				// L'erreur -2 met en evidence un probleme de surcharge du Calculator. 
 				// Interruption de la tâche et renvoie de la tache au Calculator.
 				case -2:
 					System.out.println("Surcharge du Calculator : Renvoi de la tache.");
@@ -134,7 +130,7 @@ public class Dispatcher {
 					break;
 				
 				//	L'erreur -2 met en evidence un probleme d'identification au niveau du Dispatcher.
-				// Interruption du calcul et renvoie de message à l'utilisateur.
+				// Interruption du calcul et renvoie de message a l'utilisateur.
 				case -3:
 					System.out.println("Echec de l'identification Dispatcher : Revoir les identifiants Dispatcher.");
 					System.exit(1);
@@ -153,7 +149,7 @@ public class Dispatcher {
 	// Faire un test sur les serveurs avant de passer en ModeSecure ??????
 	private void unSecureCalculation(String username, String password) {
 		
-		//Inititialisation du résultat final
+		//Inititialisation du resultat final
 		int resultat = 0;
    
 
@@ -177,49 +173,50 @@ public class Dispatcher {
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-				
 				int result = thread.resultat;
 				switch (result)
 				{
-				
-				// L'erreur -1 met en evidence un probleme de syntaxe dans le contenu du fichier (notamment sur la composition des operations).
-				// Interruption du calcul et renvoie de message à l'utilisateur.
-				case -1:
-					System.out.println("Calcul impossible : Revoir le contenu du fichier transmis.");
-					System.exit(1);
-				
-				// L'erreur -2 met en evidence un problème de surcharge du Calculator. 
-				// Interruption de la tâche et renvoie de la tache au Calculator.
-				case -2:
-					System.out.println("Surcharge du Calculator : Renvoi de la tache.");
-					thread.start();
-					listThreads.add(thread);
-					break;
-				
-				//	L'erreur -2 met en evidence un probleme d'identification au niveau du Dispatcher.
-				// Interruption du calcul et renvoie de message à l'utilisateur.
-				case -3:
-					System.out.println("Echec de l'identification Dispatcher : Revoir les identifiants Dispatcher.");
-					System.exit(1);
-					break;
-					 
-				// Le calcul s'est effectue correctement, on ajoute cette nouvelle valeur au resultat global.
-				default:
-					resultat = (resultat + result) % 4000;
+					// L'erreur -1 met en evidence un probleme de syntaxe dans le contenu du fichier (notamment sur la composition des operations).
+					// Interruption du calcul et renvoie de message a l'utilisateur.
+					case -1:
+						System.out.println("Calcul impossible : Revoir le contenu du fichier transmis.");
+						System.exit(1);
+					
+					// L'erreur -2 met en evidence un probleme de surcharge du Calculator. 
+					// Interruption de la tâche et renvoie de la tache au Calculator.
+					case -2:
+						System.out.println("Surcharge du Calculator : Renvoi de la tache.");
+						thread.start();
+						listThreads.add(thread);
+						break;
+					
+					// L'erreur -2 met en evidence un probleme d'identification au niveau du Dispatcher.
+					// Interruption du calcul et renvoie de message a l'utilisateur.
+					case -3:
+						System.out.println("Echec de l'identification Dispatcher : Revoir les identifiants Dispatcher.");
+						System.exit(1);
+						break;
+						 
+					// Le calcul s'est effectue correctement, on ajoute cette nouvelle valeur au resultat global.
+					default:
+						resultat = (resultat + result) % 4000;
 				}
 			}
 		}
 		System.out.printf("Le resultat du calcul est : ", resultat);
 	}
-	
+	/*
+	 * Mise en place de la recuperation d'acces au NameService par le RMI, sous le format stub.
+	 */	
 	private NameServiceInterface loadNameServiceStub(String hostname) {
 		NameServiceInterface stub = null;
+		// Le port du NameService est defini arbitrairement a 5000.
 		try {
 			Registry registry = LocateRegistry.getRegistry(hostname,5000);
 			stub = (NameServiceInterface) registry.lookup("nameservice");
 		} catch (NotBoundException e) {
 			System.out.println("Erreur: Le nom '" + e.getMessage()
-					+ "' n'est pas défini dans le registre.");
+					+ "' n'est pas defini dans le registre.");
 		} catch (AccessException e) {
 			System.out.println("Erreur: " + e.getMessage());
 		} catch (RemoteException e) {
@@ -228,11 +225,13 @@ public class Dispatcher {
 		return stub;
 	}
 	
+	/*
+	 * Mise en place de la recuperation d'acces aux Calculators par le RMI, sous le format stub + capacite disponible dans un HashMap.
+	 */
 	private HashMap<CalculatorInterface, Integer> loadCalculatorStub(ArrayList<Configuration> calculatorInformation) {
 		int capac = 0;
 		CalculatorInterface stub = null;
 		HashMap<CalculatorInterface, Integer> localStub = new HashMap<CalculatorInterface, Integer>();
-		// En cours
 		for (Configuration conf : calculatorInformation) {
 			try {
 				Registry registry = LocateRegistry.getRegistry(conf.calculatorIP,conf.calculatorPort);
@@ -241,7 +240,7 @@ public class Dispatcher {
 				localStub.put(stub,capac);
 			} catch (NotBoundException e) {
 				System.out.println("Erreur: Le nom '" + e.getMessage()
-						+ "' n'est pas défini dans le registre.");
+						+ "' n'est pas defini dans le registre.");
 			} catch (AccessException e) {
 				System.out.println("Erreur: " + e.getMessage());
 			} catch (RemoteException e) {
