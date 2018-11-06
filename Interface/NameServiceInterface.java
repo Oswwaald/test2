4f24bdc1a31f74fcea9aee6828b6efdef6bb31d7
Oswwaald <43680214+Oswwaald@users.noreply.github.com>
2018-11-06 10:07:33 -0500

    Update NameServiceInterface.java

--- Interface/NameServiceInterface.java
+++ Interface/NameServiceInterface.java
@@ -1,11 +1,11 @@
 package ca.polymtl.inf8480.tp2.shared;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface NameServiceInterface extends Remote {
 	boolean verifyDispatcher(String username, String password) throws RemoteException;
-	HashMap<String, Configuration> getCalculators(String username, String password) throws RemoteException;
-	void setCalculator(String name, Configuration configuration) throws RemoteException ;
+	ArrayList<Configuration> getCalculators(String username, String password) throws RemoteException;
+	void setCalculator(Configuration configuration) throws RemoteException ;
 }
